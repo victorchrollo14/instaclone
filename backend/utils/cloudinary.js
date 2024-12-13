2cee7fd38ad5e2706e996939eafd845813f1e872
victor <chrollolucilfer1402@gmail.com>
2024-12-13 23:17:36 +0530

    initial commit changed some stuff

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,16 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config({});
+
+console.log(
+  process.env.CLOUD_NAME,
+  process.env.API_KEY,
+  process.env.API_SECRET,
+);
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+export default cloudinary;
+
